d55c29ec528bc541ec03f7fe1c0befa659444862
ro-mak <eee127@yandex.ru>
2017-10-24 21:43:50 +0300

    - changed hello message - added an image

--- app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
+++ app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
@@ -3,11 +3,15 @@ package ru.makproductions.androidlevel_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.geekbrains);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 }
