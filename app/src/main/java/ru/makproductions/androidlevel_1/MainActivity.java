c1df45e78dad84836f064d491bdd4d0f368e3728
ro-mak <eee127@yandex.ru>
2017-11-06 16:18:54 +0300

    - Разобрался с SharedPreferences и добавил сохранение города.

--- app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
+++ app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.makproductions.androidlevel_1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +13,27 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String WEATHER_MESSAGE = "weather_message";
+    private static final String TOWN_NUMBER = "townNumber";
     private TextView descriptionText;
     private Button showDescriptionButton;
     private Spinner spinnerForCities;
+    private SharedPreferences saveTown;
+    private int townSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        saveTown = getPreferences(MODE_PRIVATE);
+        townSelected = saveTown.getInt(TOWN_NUMBER, 0);
+
         setContentView(R.layout.activity_main);
+
         descriptionText = (TextView) findViewById(R.id.textview_description);
-        showDescriptionButton = (Button)findViewById(R.id.show_description_button);
-        spinnerForCities = (Spinner)findViewById(R.id.spinner_colours);
+        showDescriptionButton = (Button) findViewById(R.id.show_description_button);
+        spinnerForCities = (Spinner) findViewById(R.id.spinner_colours);
+        spinnerForCities.setSelection(townSelected);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.geekbrains);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
@@ -30,14 +41,20 @@ public class MainActivity extends AppCompatActivity {
         showDescriptionButton.setOnClickListener(onClickListener);
     }
 
-    View.OnClickListener onClickListener = new View.OnClickListener(){
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveTown.edit().putInt(TOWN_NUMBER, spinnerForCities.getSelectedItemPosition()).apply();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if(view.getId() == R.id.show_description_button){
+            if (view.getId() == R.id.show_description_button) {
                 String result = CitiesSpec.getWeatherDescription(MainActivity.this, spinnerForCities.getSelectedItemPosition());
                 descriptionText.setText(result);
-                Intent intent = new Intent(MainActivity.this,ShowWeather.class);
-                intent.putExtra(WEATHER_MESSAGE,result);
+                Intent intent = new Intent(MainActivity.this, ShowWeather.class);
+                intent.putExtra(WEATHER_MESSAGE, result);
                 startActivity(intent);
             }
         }
