ecdd857beaddf9e2ee609bde2efd01244ad9a1c0
Inlinebrother <eee127@yandex.ru>
2018-04-10 23:11:43 +0300

    -replaced activities with fragments

--- app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
+++ app/src/main/java/ru/makproductions/androidlevel_1/MainActivity.java
@@ -1,38 +1,16 @@
 package ru.makproductions.androidlevel_1;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.Arrays;
-import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String WEATHER_MESSAGE = "weather_message";
-    private static final String TOWN_NUMBER = "townNumber";
+public class MainActivity extends AppCompatActivity implements WeatherListListener {
+
     private static final String TAG = "HeyHOO###############";
-    private static final String PRESSURE = "PRESSURE";
-    private static final String TOMMOROW_FORECAST = "TOMMOROW_FORECAST";
-    private static final String WEEK_FORECAST = "WEEK_FORECAST";
-    private TextView descriptionText;
-    private Button showDescriptionButton;
-   // private Spinner spinnerForCities;
-    private RecyclerView recyclerView;
-    private SharedPreferences saveTown;
-    private int townSelected;
     private final int SUCCESS_CODE = 666;
 
 
@@ -40,93 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-
-        saveTown = getPreferences(MODE_PRIVATE);
-
         setContentView(R.layout.activity_main);
-
-        descriptionText = (TextView) findViewById(R.id.textview_description);
-        CheckBox checkBoxPressure = (CheckBox) findViewById(R.id.checkbox_pressure);
-        CheckBox checkBoxTommorowForecast = (CheckBox) findViewById(R.id.checkbox_tommorow_forecast);
-        CheckBox checkBoxWeekForecast = (CheckBox) findViewById(R.id.checkbox_week_forecast);
-
-        if (savedInstanceState != null) {
-            townSelected = savedInstanceState.getInt(TOWN_NUMBER);
-            descriptionText.setText(savedInstanceState.getString(WEATHER_MESSAGE));
-        } else {
-            townSelected = saveTown.getInt(TOWN_NUMBER, 0);
-            pressure = saveTown.getBoolean(PRESSURE,false);
-            checkBoxPressure.setChecked(pressure);
-            tommorowForecast = saveTown.getBoolean(TOMMOROW_FORECAST,false);
-            checkBoxTommorowForecast.setChecked(tommorowForecast);
-            weekForecast = saveTown.getBoolean(WEEK_FORECAST,false);
-            checkBoxWeekForecast.setChecked(weekForecast);
-        }
-        showDescriptionButton = (Button) findViewById(R.id.show_description_button);
-        //spinnerForCities = (Spinner) findViewById(R.id.spinner_colours);
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        RVAdapter adapter = new RVAdapter(Arrays.asList(getResources().getStringArray(R.array.cities)));
-        recyclerView.setAdapter(adapter);
-
-
-       // spinnerForCities.setSelection(townSelected);
-
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.geekbrains);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
-
-
-        checkBoxPressure.setOnClickListener(onClickListener);
-        checkBoxTommorowForecast.setOnClickListener(onClickListener);
-        checkBoxWeekForecast.setOnClickListener(onClickListener);
-        showDescriptionButton.setOnClickListener(onClickListener);
-    }
-
-         class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder>{
-                List<String> cities;
-                RVAdapter(List<String> cities){
-                    this.cities = cities;
-                }
-             @Override
-             public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                    View view = LayoutInflater.from(parent.getContext())
-                            .inflate(R.layout.category_list_item,parent,false);
-                    MyViewHolder myViewHolder = new MyViewHolder(view);
-                    view.setOnClickListener(myViewHolder);
-                 return myViewHolder;
-             }
-
-             @Override
-             public void onBindViewHolder(MyViewHolder holder, int position) {
-                    holder.city.setText(cities.get(position));
-             }
-
-             @Override
-             public int getItemCount() {
-                 return cities.size();
-             }
-
-             public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-                 TextView city;
-            MyViewHolder(View itemView){
-                super(itemView);
-                city = (TextView)itemView.findViewById(R.id.city);
-            }
-
-                 @Override
-                 public void onClick(View v) {
-                     townSelected = getAdapterPosition();
-                     result = CitiesSpec.getWeatherDescription(MainActivity.this,townSelected, pressure, tommorowForecast, weekForecast);
-                     descriptionText.setText(result);
-                     Intent intent = new Intent(MainActivity.this, ShowWeather.class);
-                     intent.putExtra(WEATHER_MESSAGE, result);
-                     startActivityForResult(intent, SUCCESS_CODE);
-                 }
-             }
     }
 
     @Override
@@ -163,56 +58,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         Log.d(TAG, "onPause");
         super.onPause();
-        saveTown.edit()
-                .putInt(TOWN_NUMBER, townSelected)
-                .putBoolean(PRESSURE,pressure)
-                .putBoolean(TOMMOROW_FORECAST,tommorowForecast)
-                .putBoolean(WEEK_FORECAST,weekForecast)
-                .apply();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        Log.d(TAG, "OnSaveInstanceState");
-        outState.putInt(TOWN_NUMBER, townSelected);
-        outState.putString(WEATHER_MESSAGE, descriptionText.getText().toString());
-        super.onSaveInstanceState(outState);
-    }
-
-    private String result = "";
-    private boolean pressure;
-    private boolean tommorowForecast;
-    private boolean weekForecast;
-    // region ClickListener
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if (view.getId() == R.id.show_description_button) {
-                result = CitiesSpec.getWeatherDescription(MainActivity.this, townSelected, pressure, tommorowForecast, weekForecast);
-                descriptionText.setText(result);
-                Intent intent = new Intent(MainActivity.this, ShowWeather.class);
-                intent.putExtra(WEATHER_MESSAGE, result);
-                startActivityForResult(intent, SUCCESS_CODE);
-            }
-            if (view.getId() == R.id.checkbox_pressure) {
-                pressure = !pressure;
-            } else if (view.getId() == R.id.checkbox_tommorow_forecast) {
-                tommorowForecast = !tommorowForecast;
-            } else if (view.getId() == R.id.checkbox_week_forecast) {
-                weekForecast = !weekForecast;
-            }
-        }
-    };
-//endregion
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == SUCCESS_CODE) {
             if (resultCode == RESULT_OK) {
-                descriptionText.setText(R.string.weather_share_result_ok);
             } else if (resultCode == RESULT_CANCELED) {
-                descriptionText.setText(R.string.weather_share_cancel);
             }
         }
+    }
+
+    @Override
+    public void onListItemClick(int id) {
+
     }
 }
---
+++ app/src/main/java/ru/makproductions/androidlevel_1/ShowWeatherFragment.java
@@ -0,0 +1,54 @@
+package ru.makproductions.androidlevel_1;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShowWeatherFragment extends Fragment {
+
+    private String weather;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_show_weather, container, false);
+        TextView showWeatherTextView = (TextView) view.findViewById(R.id.show_weather_textview);
+        showWeatherTextView.setText(weather);
+
+        Button shareWeatherButton = (Button) view.findViewById(R.id.share_weather_button);
+        shareWeatherButton.setOnClickListener(onClickListener);
+        return view;
+    }
+
+    public void setWeather(String weather){
+        this.weather = weather;
+    }
+
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.share_weather_button) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, weather);
+//                PackageManager packageManager = getPackageManager();
+//                if(!packageManager.queryIntentActivities(intent,0).isEmpty()) {
+//                    startActivity(intent);
+//                    setResult(RESULT_OK);
+//                }else{
+//                    setResult(RESULT_CANCELED);
+//                }
+//            }
+            }
+        }
+    };
+}
